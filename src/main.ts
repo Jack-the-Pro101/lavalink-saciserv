9b1fc018926e98d47de8edce8c1b2a4b010c9ec0
Jack <canadajackhuang@gmail.com>
2025-10-31 00:15:10 +0800

    Chore: cleanup for prod

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { DecryptExports, getPlayer, GetStsResponse, ResolveUrlResponse } from ".
 const allNonNulls = (requiredFields: string[]): boolean => {
   for (const field of requiredFields) {
     if (field == null) {
-      console.log("Jit in ", requiredFields, " is null");
       return false;
     }
   }
@@ -95,89 +94,3 @@ Deno.serve(
     }
   }
 );
-
-// API Specification
-// POST /decrypt_signature
-
-// Request Body:
-
-// {
-//   "encrypted_signature": "...",
-//   "n_param": "...",
-//   "player_url": "..."
-// }
-
-//     encrypted_signature (string): The encrypted signature from the video stream.
-//     n_param (string): The n parameter value.
-//     player_url (string): The URL to the JavaScript player file that contains the decryption logic.
-
-// Successful Response:
-
-// {
-//   "decrypted_signature": "...",
-//   "decrypted_n_sig": "..."
-// }
-
-// Example curl request:
-
-// curl -X POST http://localhost:8001/decrypt_signature \
-// -H "Content-Type: application/json" \
-// -H "Authorization: your_secret_token" \
-// -d '{
-//   "encrypted_signature": "...",
-//   "n_param": "...",
-//   "player_url": "https://..."
-// }'
-
-// POST /get_sts
-
-// Extracts the signature timestamp (sts) from a player script.
-
-// Request Body:
-
-// {
-//   "player_url": "..."
-// }
-
-//     player_url (string): The URL to the JavaScript player file.
-
-// Successful Response:
-
-// {
-//   "sts": "some_timestamp"
-// }
-
-// Example curl request:
-
-// curl -X POST http://localhost:8001/get_sts \
-// -H "Content-Type: application/json" \
-// -H "Authorization: your_secret_token" \
-// -d '{
-//   "player_url": "https://..."
-// }'
-
-// POST /resolve_url
-
-// Resolves a raw stream URL by handling the signature and n-parameter decryption, returning a fully constructed and ready-to-use playback URL.
-
-// Request Body:
-
-// {
-//   "stream_url": "...",
-//   "player_url": "...",
-//   "encrypted_signature": "...",
-//   "signature_key": "...",
-//   "n_param": "..."
-// }
-
-//     stream_url (string): The initial stream URL (not video url).
-//     player_url (string): The URL to the JavaScript player file.
-//     encrypted_signature (string): The encrypted signature value.
-//     signature_key (string, optional): The query parameter key to use for the decrypted signature in the final URL. Defaults to sig.
-//     n_param (string, optional): The n parameter value. If not provided, it will be extracted from the stream_url.
-
-// Successful Response:
-
-// {
-//   "resolved_url": "..."
-// }
